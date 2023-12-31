7dd463a8b37139b2e1cf3fda14d5635910b21cc6
sherzod <sherzodqadirov01@gmail.com>
2023-12-31 17:44:29 +0500

    fixed db

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,14 +1,22 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-const connectDB = async () => {
-    try {
-         await mongoose.connect(process.env.DB_URL);
-        console.log(`MongoDB Connected `);
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
+import{MongoClient, ServerApiVersion} from 'mongodb'
+const uri = "mongodb+srv://sherzod:sherzod6134@users.6rbaatv.mongodb.net/";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
 }
 export default connectDB;
-
